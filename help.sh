388a228a2f670b0a128ec5cbb31edeb10d9b30c4
borislav <b.manolov@gmail.com>
2017-06-14 16:17:51 +0200

    use a heredoc string for the program usage it is slightly easier to read than multiple echo commands

--- help.sh
+++ help.sh
@@ -1,28 +1,30 @@
-# colors
-c_red="\033[1;31m"
-c_green="\033[1;32m"
-c_reset="\033[0m"
-
 # Help section
 show_help() {
 
+	# colors
+	local c_red=$(printf '\033[1;31m')
+	local c_green=$(printf '\033[1;32m')
+	local c_reset=$(printf '\033[0m')
+
 	if [[ -n "$1" ]]; then
-		echo -e "Unknown option: '${c_red}$1${c_reset}'\n" >&2
+		echo -e "\nUnknown option: '${c_red}$1${c_reset}'\n" >&2
 	fi
 
-	echo "Available options:"
-	echo
-	echo -e "  ${c_green}--mysql-clean${c_reset}         - dump all MySQL databases ${c_red}wthout${c_reset} encryption"
-	echo -e "  ${c_green}--mysql-encrypt${c_reset}       - dump all MySQL databases and ${c_red}encrypt${c_reset} them"
-	echo -e "  ${c_green}--rsync-master${c_reset}        - rsync databases to the ${c_red}main${c_reset} backup server"
-	echo -e "  ${c_green}--rsync-secondary${c_reset}     - rsync databases to the ${c_red}secondary${c_reset} backup server"
-	echo -e "  ${c_green}--localbkp-clear${c_reset}      - clear ${c_red}localbkp${c_reset} directory"
-	echo -e "  ${c_green}--lokalbkp-check${c_reset}      - check ${c_red}localbkp${c_reset} directory and print if there are any files"
-	echo -e "  ${c_green}--localbkp-encrypt${c_reset}    - encrypt any custom files in the ${c_red}localbkp${c_reset} directory"
-	echo -e "  ${c_green}--restore-decrypt${c_reset}     - decrypt all files in ${c_red}restore${c_reset} directory"
-	echo -e "  ${c_green}--restore-clear${c_reset}       - delete all files from the ${c_red}restore${c_reset} directory"
-	echo -e "  ${c_green}--files${c_reset}               - backup files/directories from each line in ${c_red}files.conf${c_reset} without encryption"
-	echo -e "  ${c_green}--clear-logs${c_reset}          - clear ${c_red}all${c_reset} logs and monitoring checks"
-	echo
+	cat <<-USAGE
+	Available options:
+
+	  ${c_green}--mysql-clean${c_reset}         - dump all MySQL databases ${c_red}wthout${c_reset} encryption
+	  ${c_green}--mysql-encrypt${c_reset}       - dump all MySQL databases and ${c_red}encrypt${c_reset} them
+	  ${c_green}--rsync-master${c_reset}        - rsync databases to the ${c_red}main${c_reset} backup server
+	  ${c_green}--rsync-secondary${c_reset}     - rsync databases to the ${c_red}secondary${c_reset} backup server
+	  ${c_green}--localbkp-clear${c_reset}      - clear ${c_red}localbkp${c_reset} directory
+	  ${c_green}--lokalbkp-check${c_reset}      - check ${c_red}localbkp${c_reset} directory and print if there are any files
+	  ${c_green}--localbkp-encrypt${c_reset}    - encrypt any custom files in the ${c_red}localbkp${c_reset} directory
+	  ${c_green}--restore-decrypt${c_reset}     - decrypt all files in ${c_red}restore${c_reset} directory
+	  ${c_green}--restore-clear${c_reset}       - delete all files from the ${c_red}restore${c_reset} directory
+	  ${c_green}--files${c_reset}               - backup files/directories from each line in ${c_red}files.conf${c_reset} without encryption
+	  ${c_green}--clear-logs${c_reset}          - clear ${c_red}all${c_reset} logs and monitoring checks
+
+USAGE
 	exit 1
 }
