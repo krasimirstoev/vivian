d63bb85f42809365973239911462045889570cba
borislav <b.manolov@gmail.com>
2017-06-14 09:15:23 +0200

    move cleaner functions to a separate script

---
+++ cleaner.sh
@@ -0,0 +1,21 @@
+function localbkp_clear(){
+
+	# this function will clear localbkp
+	rm -rf $vivian_localbkp/*
+	log "All files in localbkp were deleted."
+
+	# check localbkp content (for Nagios)
+	localbkp_check
+
+	# this will make log file a bit easier for reading
+	log "============================================="
+
+}
+
+function restore_clear(){
+
+	# this function will clear all files in restore
+	rm -rf $vivian_restore/*
+	log "All files in restore were deleted."
+
+}
--- vivian.sh
+++ vivian.sh
@@ -24,29 +24,8 @@ source $vivian_root/help.sh
 source $vivian_root/mysql.sh
 source $vivian_root/encryption.sh
 source $vivian_root/rsync.sh
+source $vivian_root/cleaner.sh
 source $vivian_root/logging.sh
-
-function localbkp_clear(){
-
-	# this function will clear localbkp
-	rm -rf $vivian_localbkp/*
-	log "All files in localbkp were deleted."
-
-	# check localbkp content (for Nagios)
-	localbkp_check
-
-	# this will make log file a bit easier for reading
-	log "============================================="
-
-}
-
-function restore_clear(){
-
-	# this function will clear all files in restore
-	rm -rf $vivian_restore/*
-	log "All files in restore were deleted."
-
-}
 
 function localbkp_encrypt(){
 
