e43ff6b5d57b3de89b76450ca75e7da4d9c2f930
borislav <b.manolov@gmail.com>
2017-06-16 12:52:04 +0200

    refactor rsync copying - put remote back into the remote_storage definition; create target dir if missing

--- rsync.sh
+++ rsync.sh
@@ -1,38 +1,40 @@
-# remote server paths
-vivian_remote_storage="${vivian_remote_main}/${this_server}"
-vivian_remote_storage_files="${vivian_remote_storage}/files"
-
-# remote connection
-backup_key="${vivian_root}/master_key"
-
 rsync_to_storages() {
 	local storages=("$@")
 	for storage_def in "${storages[@]}"; do
 		parts=($storage_def)
-		rsync_to_storage "${parts[0]}@${parts[1]}" ${parts[2]}
+		rsync_to_storage "${parts[0]}@${parts[1]}" ${parts[2]} "${parts[3]}"
 	done
 }
 
 rsync_to_storage() {
 	local host=$1
 	local port=$2
+	local path=$3
+
+	# remote server paths
+	local full_path="$path/$this_server"
+	local full_path_files="${full_path}/files"
+	local backup_key="${vivian_root}/master_key"
 
 	# let's move databases
-	rsync_files $host:$vivian_remote_storage $port $backup_key "${vivian_localbkp}/*.pi"
+	rsync_files "$host" $port "$backup_key" "$full_path" "${vivian_localbkp}/*.pi"
 
 	# if we have any files, we will move them
 	if [ -d "$vivian_localbkp_files" ]; then
-		rsync_files $host:$vivian_remote_storage_files $port $backup_key "${vivian_localbkp_files}"
+		rsync_files "$host" $port "$backup_key" "$full_path_files" "${vivian_localbkp_files}"
 	fi
 }
 
 rsync_files() {
-	local remote_path=$1
-	local remote_ssh_port=$2
+	local host=$1
+	local ssh_port=$2
 	local ssh_key=$3
-	local local_files=$4
+	local remote_path=$4
+	local local_files=$5
 	rsync_key_create $ssh_key
-	rsync -avz --progress -e "ssh -p $remote_ssh_port -i $ssh_key" $local_files $remote_path
+	# first make sure that the target directory exists
+	ssh -p $ssh_port -i $ssh_key "$host" mkdir -p "$remote_path"
+	rsync -avz --progress -e "ssh -p $ssh_port -i $ssh_key" $local_files "$host:$remote_path"
 	rsync_key_destroy $ssh_key
 }
 
