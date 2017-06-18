364c5a910093ec3bab394611636a5de3c177368f
borislav <b.manolov@gmail.com>
2017-06-18 09:33:33 +0200

    update usage help - rsync command

--- help.sh
+++ help.sh
@@ -15,8 +15,7 @@ show_help() {
 
 	  ${c_green}--mysql-clean${c_reset}         - dump all MySQL databases ${c_red}wthout${c_reset} encryption
 	  ${c_green}--mysql-encrypt${c_reset}       - dump all MySQL databases and ${c_red}encrypt${c_reset} them
-	  ${c_green}--rsync-master${c_reset}        - rsync databases to the ${c_red}main${c_reset} backup server
-	  ${c_green}--rsync-secondary${c_reset}     - rsync databases to the ${c_red}secondary${c_reset} backup server
+	  ${c_green}--rsync${c_reset}               - rsync databases with the ${c_red}remote storages${c_reset}
 	  ${c_green}--localbkp-encrypt${c_reset}    - encrypt any custom files in the ${c_red}localbkp${c_reset} directory
 	  ${c_green}--restore-decrypt${c_reset}     - decrypt all files in ${c_red}restore${c_reset} directory
 	  ${c_green}--files${c_reset}               - backup files/directories from each line in ${c_red}files.conf${c_reset} without encryption
